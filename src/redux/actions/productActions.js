e6854388c54cbebb37529e8703902b1dc42e7a23
Mich <michola3636@gmail.com>
2021-03-02 22:16:04 +0000

    setup17

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -38,7 +38,7 @@ export const getProductDetails = (id) => async (dispatch) => {
 
     try{
         dispatch({type: actionTypes.GET_PRODUCT_DETAILS_REQUEST})
-        const { data } = await axios.get(`/api/products/${id}`)
+        const { data } = await axios.get(`https://thestores.herokuapp.com/api/products/${id}`)
 
         dispatch({
             type: actionTypes.GET_PRODUCT_DETAILS_SUCCESS,
